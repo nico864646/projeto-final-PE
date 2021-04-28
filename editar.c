98c47dd3cc4215d4e03bfcf3d768f4264b131518
Nicolas Moraes <nicos.moraes@hotmail.com>
2021-04-28 14:45:43 -0300

    Editar cadastros.

--- editar.c
+++ editar.c
@@ -3,7 +3,7 @@
 
 void editar_motorista(){
 
-    int i = 0, aux_numViagem = 0;
+    int opcao_motorista = 0, aux_numViagem = 0;
 
     system("cls");
     printf("\n\t\tEDITAR MOTORISTAS");
@@ -13,25 +13,25 @@ void editar_motorista(){
     do{
         printf("\n\tQual o número do ID motorista: ");
         printf("\n\tSelecionar: ");
-        scanf("%d", &i);
-        i--;
-        aux_numViagem = registroM[i].num_viagens;
+        scanf("%d", &opcao_motorista);
+        opcao_motorista--;
+        aux_numViagem = registroM[opcao_motorista].num_viagens;
 
-        if(registroM[i].status == 2 || registroM[i].status == 0){
+        if(registroM[opcao_motorista].status != 1){
             printf("\n\tNão é possivel alterar dados deste motorista!\n");
         }
         else
             break;
-    }while(registroM[i].status != 1);
+    }while(registroM[opcao_motorista].status != 1);
 
-            insere_informacao_motoristas(i);
-            registroM[i].num_viagens = aux_numViagem;
+            insere_informacao_motoristas(opcao_motorista);
+            registroM[opcao_motorista].num_viagens = aux_numViagem;
             salvar_motorista();
 }
 
 void editar_veiculo(){
 
-    int i = 0, opcao_edita_veiculo = 0;
+    int opcao_veiculo = 0, opcao_edita_veiculo = 0;
 
     system("cls");
     printf("\n\t\tEDITAR VEÍCULO");
@@ -45,21 +45,21 @@ void editar_veiculo(){
 
     switch (opcao_edita_veiculo){
         case 1:
-            i = escolhe_veiculo();
-            insere_cadastro_veiculos(i);
+            opcao_veiculo = escolhe_veiculo();
+            insere_cadastro_veiculos(opcao_veiculo);
             salvar_veiculo();
             break;
 
         case 2:
             escolhe_veiculo();
 
-            if(registroV[i].status == 1){
+            if(registroV[opcao_veiculo].status == 1){
                 printf("\n\tTransferindo veículo para manutenção..\n.");
-                registroV[i].status = 3;
+                registroV[opcao_veiculo].status = 3;
             }
-            else if(registroV[i].status == 3){
+            else if(registroV[opcao_veiculo].status == 3){
                 printf("\n\tTransferindo veículo para o pátio...\n");
-                registroV[i].status = 1;
+                registroV[opcao_veiculo].status = 1;
             }
             else{
                 printf("\n\tVeículo em viagem, impossível transferi-lo para manutenção\n");
@@ -74,17 +74,17 @@ void editar_veiculo(){
 }
 
 int escolhe_veiculo(){
-    int i = 0;
+    int opcao_veiculo = 0;
     printf("\n\tID veículo: ");
     printf("\n\tSelecionar: ");
-    scanf("%d", &i);
-    i--;
-    return i;
+    scanf("%d", &opcao_veiculo);
+    opcao_veiculo--;
+    return opcao_veiculo;
 }
 
 void editar_viagem(){
 
-    int opcao = 0, opcao_veiculo = 0, opcao_motorista = 0, i = 0, aux_iDIA = 0, aux_iMES = 0, aux_iANO = 0;
+    int opcao = 0, opcao_veiculo = 0, opcao_motorista = 0, opcao_viagem = 0, aux_iDIA = 0, aux_iMES = 0, aux_iANO = 0;
 
     system("cls");
     printf("\n\t\tEDITAR VIAGENS");
@@ -101,7 +101,7 @@ void editar_viagem(){
 
         switch(opcao){
             case 1:
-                i = escolhe_viagem();
+                opcao_viagem = escolhe_viagem();
                 printf("\n\tVeículos disponíveis: ");
                 consultar_vDisponivel();
 
@@ -115,13 +115,13 @@ void editar_viagem(){
 
                 }while(registroV[opcao_veiculo].status != 1);
 
-                registroViagem[i].id_veiculo = opcao_veiculo;
+                registroViagem[opcao_viagem].id_veiculo = opcao_veiculo;
                 salvar_veiculo();
                 salvar_viagem();
             break;
 
             case 2:
-                i = escolhe_viagem();
+                opcao_viagem = escolhe_viagem();
                 printf("\n\tMotoristas disponíveis: ");
                 consultar_motorista_disponivel();
 
@@ -136,52 +136,53 @@ void editar_viagem(){
                 }while(registroM[opcao_motorista].status != 1);
 
                     //Motorista anterior volta estar disponivel para viagens
-                    registroM[registroViagem[i].id_motorista].status = 1;
+                    registroM[registroViagem[opcao_viagem].id_motorista].status = 1;
 
                     //Atualiza o ID do motorista da viagem
-                    registroViagem[i].id_motorista = opcao_motorista;
+                    registroViagem[opcao_viagem].id_motorista = opcao_motorista;
 
                     //Novo motorista muda status para "Em Viagem"
-                    registroM[registroViagem[i].id_motorista].status = 2;
+                    registroM[registroViagem[opcao_viagem].id_motorista].status = 2;
                 salvar_motorista();
                 salvar_viagem();
             break;
 
             case 3:
-                i = escolhe_viagem();
+                opcao_viagem = escolhe_viagem();
 
-                aux_iDIA = registroViagem[i].data_inicio_dia;
-                aux_iMES = registroViagem[i].data_inicio_mes;
-                aux_iANO = registroViagem[i].data_inicio_ano;
+                aux_iDIA = registroViagem[opcao_viagem].data_inicio_dia;
+                aux_iMES = registroViagem[opcao_viagem].data_inicio_mes;
+                aux_iANO = registroViagem[opcao_viagem].data_inicio_ano;
+
                 printf("\n\tFinalizar Viagem: \n");
 
-                int j = registroViagem[i].id_veiculo;
+                int j = registroViagem[opcao_viagem].id_veiculo;
 
                 printf("\n\t*DATA FIM (dd/mm/aa): ");
-                scanf("%d/%d/%d", &registroViagem[i].data_fim_dia, &registroViagem[i].data_fim_mes, &registroViagem[i].data_fim_ano);
+                scanf("%d/%d/%d", &registroViagem[opcao_viagem].data_fim_dia, &registroViagem[opcao_viagem].data_fim_mes, &registroViagem[opcao_viagem].data_fim_ano);
 
-                registroViagem[i].duracao_dias = dist_dias(i);
+                registroViagem[opcao_viagem].duracao_dias = dist_dias(opcao_viagem);
 
                 printf("\n\t*HORA FIM (hh:mm): ");
-                scanf("%d:%d", &registroViagem[i].hora_fim_hh, &registroViagem[i].hora_fim_mm);
+                scanf("%d:%d", &registroViagem[opcao_viagem].hora_fim_hh, &registroViagem[opcao_viagem].hora_fim_mm);
 
                 //Atualiza status viagem
-                registroViagem[i].status = 2;
+                registroViagem[opcao_viagem].status = 2;
 
                 //Atualiza status motorista
-                registroM[registroViagem[i].id_motorista].num_viagens = registroM[registroViagem[i].id_motorista].num_viagens + 1;
-                registroM[registroViagem[i].id_motorista].status = 1;
+                registroM[registroViagem[opcao_viagem].id_motorista].num_viagens = registroM[registroViagem[opcao_viagem].id_motorista].num_viagens + 1;
+                registroM[registroViagem[opcao_viagem].id_motorista].status = 1;
 
                 //Atualiza status veiculo
-                registroV[i].quilometragem = registroV[j].quilometragem + registroViagem[i].percorrido;
+                registroV[opcao_viagem].quilometragem = registroV[j].quilometragem + registroViagem[opcao_viagem].percorrido;
 
-                registroV[i].status = 1;
+                registroV[opcao_viagem].status = 1;
 
                 printf("\n\tViagem realizada com sucesso!\n");
 
-                registroViagem[i].data_inicio_dia = aux_iDIA; 
-                registroViagem[i].data_inicio_mes = aux_iMES;  
-                registroViagem[i].data_inicio_ano = aux_iANO; 
+                registroViagem[opcao_viagem].data_inicio_dia = aux_iDIA; 
+                registroViagem[opcao_viagem].data_inicio_mes = aux_iMES;  
+                registroViagem[opcao_viagem].data_inicio_ano = aux_iANO; 
 
                 //Salvar
                 salvar_veiculo();
